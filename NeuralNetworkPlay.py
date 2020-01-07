af5c392fc525a69e29890d2c41d8458bd6b254e8
Geraldo Luiz <geraldorj-2010@hotmail.com>
2020-01-06 21:57:17 -0300

    Refactored Intelect basically

--- NeuralNetworkPlay.py
+++ NeuralNetworkPlay.py
@@ -19,27 +19,32 @@ Main build:
 
 if __name__ == "__main__":
   from Darwin import Darwin
-  darwin = Darwin(offspring_num = 100,
-                    generations = 50,
-                    sizes=[20,21,27,4],
-                    num_parents = 64,
-                    mutation_rate = 7,
-                    crossing_algorithm = "uniform",
-                    saving_txt = True,
-                    saving_csv = True,
-                    saving_dna = True,
-                    saveDnaThreshold = None,
+                    
+  darwin = Darwin( 
 
 
+    # Hyperparameters
+    offspring_num = 100,
+    generations = 50,
+    sizes=[20,21,27,4],
+    num_parents = 64,
+    mutation_rate = 7,
+    crossing_algorithm = "uniform",
 
+    # Saving
+    saving_txt = True,
+    saving_csv = True,
+    saving_dna = True,
+    saveDnaThreshold = None,
 
-                    cluster_id = "Darwin 1",
-                    tty = None,              #If you want to redirect the
-                                             #   output to a specific terminal,
-                                             #  useful when dealing with threads.
+    #  If you want to redirect the
+    #  output to a specific terminal,
+    #  useful when dealing with threads.
+    cluster_id = "Darwin 1",
+    tty = None,              
 
-
-                    loadDnaPath = "GenTests/Gen test - 2020-01-06 - 14:19:33-Darwin 1-2-109.0.npy") #Load parents from another training
+    #Load parents from another training
+    loadDnaPath = "") 
 
   darwin.main()
   
