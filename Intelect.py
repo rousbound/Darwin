338e363e8e31f8bea7097a475e35d6772ecddd10
Geraldo Luiz <geraldorj-2010@hotmail.com>
2020-01-08 11:31:03 -0300

    Minor change, added description to softmax function, changed name of folder with bests DNA's from TrainedDNA to BestDNA.

--- Intelect.py
+++ Intelect.py
@@ -8,6 +8,7 @@ class Network(object):
     # total_weight = 252 ( 8*9 + 9*15 + 15 * 3)
     # sizePairs = [[9,8],[15,9],[3,15]]
     # synapsesOfEachLayer = [72,125,45]
+    # layerWeights = [72weights,125weights,45weights]
     # synapsesOfEachNeuron = [[9,8weights],[15,9weights],[3,15weights]]
 
   
@@ -49,6 +50,14 @@ class Network(object):
         a = softmax(z)
         return a
 
+# The softmax function is an activation function that turns numbers into probabilities which sum to
+# one. Softmax function turns logits [2.0, 1.0, 0.1] into probabilities [0.7, 0.2, 0.1], and the
+# probabilities sum to 1
+
+# ndarray.T: Same as self.transpose(), except that self is returned if self.ndim < 2.
+# np.exp(x): e**x
+# reshape(-1,1): reshape matrix with one column and an unspecified number of rows that make the
+# reshaping compatible
 def softmax(z):
     s = np.exp(z.T) / np.sum(np.exp(z.T), axis=1).reshape(-1, 1)
     return s
